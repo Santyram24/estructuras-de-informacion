df3f50b05acdfad413be1ae2e400743401f99d98
santy <santy@santy-HP-Pavilion-g4-Notebook-PC>
2021-09-21 10:34:48 -0500

    -----taller3

--- src/main/java/co/edu/ucundinamarca/taller3/MainSucesion.java
+++ src/main/java/co/edu/ucundinamarca/taller3/MainSucesion.java
@@ -5,6 +5,7 @@
  */
 package co.edu.ucundinamarca.taller3;
 
+import java.util.Scanner;
 import org.apache.log4j.Logger;
 
 /**
@@ -13,8 +14,13 @@ import org.apache.log4j.Logger;
  */
 public class MainSucesion {
      static Logger log = Logger.getLogger(co.edu.ucundinamarca.taller3.MainSucesion.class.getName());//invocar el logger
-    public static void main(String[] args) {
-        log.info(Sucesion.sucesion(10));//invocar le metodo de sucesion en la clase Sucesion
+    
+     public static void main(String[] args) {
+         Scanner leer= new Scanner(System.in);
+        int n;//varaible de sucesion
+        log.info("Digite el numero de sucuesion");
+        n=leer.nextInt();
+        log.info(Sucesion.sucesion(n));//invocar le metodo de sucesion en la clase Sucesion
         Sucesion.mostrarContador();//invocar el metodo mostrarContador() de la clase Sucesion
     }
 }
